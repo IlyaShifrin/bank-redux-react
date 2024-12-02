61effbf0ac925c306ee5752b7effd46b5861881a
Ilya <drunktrumpet1@gmail.com>
2024-12-02 19:38:00 +0200

    Complete custom redux

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,11 @@ import Operation from "./components/Operation.jsx";
 import {useState} from "react";
 
 function App() {
-    const [balance, setBalance] = useState(0);
-
-    const deposit = sum => {
-        setBalance(prevBalance => prevBalance + sum);
-    }
-
-    const withDraw = sum => {
-        setBalance(prevBalance => prevBalance < sum ? prevBalance : prevBalance - sum);
-    }
 
   return (
     <div>
-        <Balance balance={balance}/>
-        <Operation deposit={deposit} withDraw={withDraw}/>
+        <Balance/>
+        <Operation/>
     </div>
   )
 }
