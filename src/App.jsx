20fa2679b8df852db7cec6105cc464a124a23a4d
Ilya <drunktrumpet1@gmail.com>
2024-12-02 20:46:14 +0200

    Add react-redux and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
 import Balance from "./components/Balance.jsx";
 import Operation from "./components/Operation.jsx";
-import {useState} from "react";
 
 function App() {
 
   return (
-    <div>
+    <div className={'min-h-screen'}>
         <Balance/>
         <Operation/>
     </div>
