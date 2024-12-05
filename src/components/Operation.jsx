1a1966c5e761249c3d47e60c79ac58e78ed8d14c
Ilya <drunktrumpet1@gmail.com>
2024-12-05 20:48:01 +0200

    Add fetchQuote to components

--- src/components/Operation.jsx
+++ src/components/Operation.jsx
@@ -1,26 +1,47 @@
 import {useState} from "react";
 import {deposit, withDraw} from "../actions/accountActions.js";
 import {useDispatch} from "react-redux";
+import {putQuote} from "../actions/quoteAction.js";
 
 const Operation = () => {
     const [sum, setSum] = useState(1);
     const dispatch = useDispatch();
 
+    const fetchQuote = () => {
+        dispatch(putQuote('Pending...'));
+        fetch('https://api.gameofthronesquotes.xyz/v1/random ')
+            .then(res => res.json())
+            .then(data => dispatch(putQuote(data.sentence)))
+            .catch(() => putQuote('Failed to fetch quote data'));
+    }
+
     return (
-        <div className={'flex justify-center'}>
-            <button
-                className={'bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg text-lg'}
-                onClick={() => dispatch(withDraw(sum))}>Withdraw</button>
-            <input
-                className={'text-center border rounded-lg'}
-                type={'number'}
-                onChange={e => setSum(+e.target.value)}
-                value = {sum}
-            />
-            <button
-                className={'bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg text-lg'}
-                onClick={() => dispatch(deposit(sum))}>Deposit</button>
-        </div>
+        <>
+            <div className={'flex justify-center'}>
+                <button
+                    className={'bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg text-lg'}
+                    onClick={() => dispatch(withDraw(sum))}>Withdraw
+                </button>
+                <input
+                    className={'text-center border rounded-lg'}
+                    type={'number'}
+                    onChange={e => setSum(+e.target.value)}
+                    value={sum}
+                />
+                <button
+                    className={'bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded-lg text-lg'}
+                    onClick={() => dispatch(deposit(sum))}>
+                    Deposit
+                </button>
+            </div>
+            <div className={'flex justify-center'}>
+                <button
+                    onClick={fetchQuote}
+                    className={'ml-7 bg-blue-300 hover:bg-blue-600 text-white font-bold py-2 px-9 rounded-md mt-1'}
+                >Get Quote</button>
+            </div>
+        </>
+
     );
 };
 
